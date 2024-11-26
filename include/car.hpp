3c97ef2a01a5f208167aeb11c6a381161d893daa
Demian <demianvparenti@hotmail.com>
2024-11-26 18:38:31 -0300

    acomodacion del codigo para consistencia multi-archivo

--- include/car.hpp
+++ include/car.hpp
@@ -3,16 +3,15 @@
 using namespace std;
 #include "vehicle.hpp"
 
-
 class Car : public Vehicle{
-    public:
-    Car(string engine);
-    ~Car();
-    string getEngine() const;
-
     protected:
     string engine;
     void start() override;
     void stop() override;
     void refuel() override;
+
+    public:
+    Car(string engine);
+    ~Car();
+    string getEngine() const;
 };
--- include/moto.hpp
+++ include/moto.hpp
@@ -4,14 +4,14 @@ using namespace std;
 #include "vehicle.hpp"
 
 class Moto : public Vehicle{
-    public:
-    Moto(string engine);
-    ~Moto();
-    string getEngine() const;
-
     protected:
     string engine;
     void start() override;
     void stop() override;
     void refuel() override;
+    
+    public:
+    Moto(string engine);
+    ~Moto();
+    string getEngine() const;
 };
